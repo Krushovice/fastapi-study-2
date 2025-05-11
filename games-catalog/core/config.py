8e44130359abd5ecb218841608c0a0e815f1f0cd
Krushovice <kickstar69@yandex.ru>
2025-05-11 09:56:07 +0300

    update config

--- games-catalog/core/config.py
+++ games-catalog/core/config.py
@@ -1,4 +1,6 @@
+import logging
 from pathlib import Path
+from typing import Literal
 
 from pydantic import BaseModel
 from pydantic_settings import (
@@ -6,20 +8,52 @@ from pydantic_settings import (
     SettingsConfigDict,
 )
 
+LOG_DEFAULT_FORMAT = (
+    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+)
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+class LoggingConfig(BaseModel):
+    log_level: Literal[
+        "debug",
+        "info",
+        "warning",
+        "error",
+        "critical",
+    ] = "info"
+    log_format: str = LOG_DEFAULT_FORMAT
+
+    @property
+    def log_level_value(self) -> int:
+        return logging.getLevelNamesMapping()[self.log_level.upper()]
+
+
 class DBConfig(BaseModel):
     url: str = BASE_DIR / "db.sqlite3"
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class Settings(BaseSettings):
-    db: DBConfig = DBConfig()
-
     model_config = SettingsConfigDict(
         case_sensitive=False,
     )
+
+    db: DBConfig = DBConfig()
+    logging: LoggingConfig = LoggingConfig()
 
 
 settings = Settings()
