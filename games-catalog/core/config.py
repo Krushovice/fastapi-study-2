f850c58e1621ada29497505534f76a411166a4eb
Krushovice <kickstar69@yandex.ru>
2025-05-11 10:32:43 +0300

    update config

--- games-catalog/core/config.py
+++ games-catalog/core/config.py
@@ -33,7 +33,7 @@ class LoggingConfig(BaseModel):
 
 
 class DBConfig(BaseModel):
-    url: str = f"sqlite+aiosqlite:/// {DB_PATH}"
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
     echo: bool = False
     echo_pool: bool = False
     pool_size: int = 50
