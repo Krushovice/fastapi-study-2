bad8e8c2353b80a4c41b314383ae41ff4e384554
Krushovice <kickstar69@yandex.ru>
2025-05-13 12:43:14 +1000

    create game schemas

---
+++ games-catalog/api/schemas/game.py
@@ -0,0 +1,29 @@
+from datetime import date
+
+from fastapi_jsonapi.schema_base import BaseModel
+from pydantic import ConfigDict
+
+
+class GameAttributesSchema(BaseModel):
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
+    title: str
+    description: str
+    release_date: date | None = None
+
+
+class GameBaseSchema(GameAttributesSchema):
+    pass
+
+
+class GameSchema(GameBaseSchema):
+    id: int
+
+
+class GameInSchema(GameBaseSchema):
+    pass
+
+
+class GamePatchSchema(GameBaseSchema):
+    pass
