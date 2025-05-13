dc55fe15efb67dee85938217be83c5ccbd3cba8c
Krushovice <kickstar69@yandex.ru>
2025-05-13 12:43:53 +1000

    update create app func

--- games-catalog/create_app.py
+++ games-catalog/create_app.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse as JSONResponse
+
+from app_lifespan import lifespan
 
 
 def create_app() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(
+        title="FastAPI and SQLAlchemy",
+        lifespan=lifespan,
+        debug=True,
+        default_response_class=JSONResponse,
+        docs_url="/docs",
+        openapi_url="/openapi.json",
+    )
     return app
