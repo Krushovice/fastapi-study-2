d84d493660381f28cd334222e9751f525ab125cb
Krushovice <kickstar69@yandex.ru>
2025-05-14 15:10:32 +1000

    add strict title length

--- games-catalog/api/schemas/game.py
+++ games-catalog/api/schemas/game.py
@@ -1,14 +1,18 @@
 from datetime import date
+from typing import Annotated
 
+from annotated_types import MaxLen, MinLen
 from fastapi_jsonapi.schema_base import BaseModel
 from pydantic import ConfigDict
+
+title_type = Annotated[str, MinLen(1), MaxLen(120)]
 
 
 class GameAttributesSchema(BaseModel):
     model_config = ConfigDict(
         from_attributes=True,
     )
-    title: str
+    title: title_type
     description: str
     platforms: list[str]
     image_url: str
@@ -24,7 +28,7 @@ class GameCreateSchema(GameBaseSchema):
 
 
 class GameUpdateSchema(GameBaseSchema):
-    title: str | None = None
+    title: title_type | None = None
     description: str | None = None
     platform: list[str] | None = None
     image_url: str | None = None
