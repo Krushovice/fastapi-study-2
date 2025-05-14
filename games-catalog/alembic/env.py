b8e258b634e9cb4be5ed263ed8883d6aa2ffb94d
Krushovice <kickstar69@yandex.ru>
2025-05-14 23:10:08 +1000

    fix type error

--- games-catalog/alembic/env.py
+++ games-catalog/alembic/env.py
@@ -23,7 +23,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 target_metadata = models.Base.metadata
 
-config.set_main_option("sqlalchemy.url", settings.db.url)
+config.set_main_option("sqlalchemy.url", str(settings.db.url))
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
