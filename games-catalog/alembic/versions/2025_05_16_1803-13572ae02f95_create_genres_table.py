543fae12681476ef0ea26b817eff7e1f214e4ae3
Krushovice <kickstar69@yandex.ru>
2025-05-16 18:12:19 +1000

    create a genre table with citext extension

---
+++ games-catalog/alembic/versions/2025_05_16_1803-13572ae02f95_create_genres_table.py
@@ -0,0 +1,56 @@
+"""create genres table
+
+Revision ID: 13572ae02f95
+Revises: 2f461e7ecdff
+Create Date: 2025-05-16 18:03:34.131243
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+# revision identifiers, used by Alembic.
+revision: str = "13572ae02f95"
+down_revision: Union[str, None] = "2f461e7ecdff"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute("CREATE EXTENSION citext;")
+    op.create_table(
+        "genres",
+        sa.Column(
+            "id",
+            sa.Integer(),
+            sa.Identity(always=True),
+            nullable=False,
+        ),
+        sa.Column(
+            "name",
+            postgresql.CITEXT(length=20),
+            nullable=False,
+        ),
+        sa.Column(
+            "description",
+            sa.Text(),
+            server_default="",
+            nullable=False,
+        ),
+        sa.PrimaryKeyConstraint(
+            "id",
+            name=op.f("pk_genres"),
+        ),
+        sa.UniqueConstraint(
+            "name",
+            name=op.f("uq_genres_name"),
+        ),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("genres")
